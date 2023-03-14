0c6c787ed58609f766982c6928fc2781c3d5819f
Your Name <you@example.com>
2023-03-14 14:59:50 -0300

    criando protocolos de usuarios

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('teste')
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Servidor iniciado na porta ${port}`));
+
+app.get("/", (req, res) => {
+  res.send("ola mundo");
+});
